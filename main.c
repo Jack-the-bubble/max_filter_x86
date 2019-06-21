1e52e00381b90327a5c4578cf5df66efd79cd7ca
PinPoint <ms1b.139@gmail.com>
2019-06-21 17:25:48 +0200

    final version

--- main.c
+++ main.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-#include <math.h>
 #include <allegro5/allegro5.h>
 #include <allegro5/bitmap.h>
 #include <allegro5/allegro_image.h>
@@ -12,96 +9,65 @@ ALLEGRO_BITMAP *image = NULL;
 ALLEGRO_LOCKED_REGION *region = NULL;
 
 extern void max_filt(unsigned char* input, unsigned char* output, int h, int w, int m);
+void init_pix_format();
 
-extern void max_filt_u(__uint8_t* in, __uint8_t* out, int w, int h,int m);
-
-int main() {
-
-//	init library
-//	al_set_new_bitmap_format(ALLEGRO_PIXEL_FORMAT_RGB_888);
-//	if (!al_init()) // error
-//	{
-//		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!",
-//								   NULL, ALLEGRO_MESSAGEBOX_ERROR);
-//		exit(1);
-//	}
-//
-//	if (!al_init_image_addon()) // error
-//	{
-//		al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!",
-//								   NULL, ALLEGRO_MESSAGEBOX_ERROR);
-//		exit(1);
-//	}
-//
-//	//	get image from user
-//	al_destroy_bitmap(image);
-//	image = NULL;
-//	char name[100];
-//	do
-//	{
-//		printf("Podaj nazwe pliku: ");
-//		scanf("%s", name);
-//		image = al_load_bitmap(name);
-//	} while (!image);
-//
-//	int width = al_get_bitmap_width(image);
-//	int height = al_get_bitmap_height(image);
-//
-////	init display
-//	if (display)
-//		al_destroy_display(display);
-//
-//	display = al_create_display(width, height - 1);
-//
-//	if (!display)	// error
-//	{
-//		al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
-//								   NULL, ALLEGRO_MESSAGEBOX_ERROR);
-//		exit(1);
-//	}
-//
-//
-//	int widthInBytes = width * 3;
-//	unsigned char *input_buffer = (unsigned char *)malloc(height * widthInBytes);
-//
-//	region = al_lock_bitmap(image,ALLEGRO_PIXEL_FORMAT_RGB_888, ALLEGRO_LOCK_READWRITE);
-//	memcpy(input_buffer, ((unsigned char *)(region->data)) - (height - 1) * widthInBytes, height * widthInBytes);
-//	al_unlock_bitmap(image);
-//
-
-
-//	max_filt(input_buffer, (unsigned char *)(region->data) - (height - 1) * widthInBytes, width, height, 1);
-	__uint8_t test_tab[48];
-	__uint8_t test_tab_2[48];
-	for (__uint8_t i = 0; i < 48; ++i) {
-		if (i % 12 == 0)
-			printf("\n");
-		test_tab[i]= i/3;
-		test_tab_2[i] = i/3;
-		printf("%d ", test_tab[i]);
+int main(int argc, char *argv[]){
+	init_pix_format();
+	char comm[10];
+	char pic[20];
+	while(true)	{
+		printf("type file name with extention ");
+		scanf("%s", pic);
+		image = al_load_bitmap(pic);
+		if (image)
+			break;
 	}
 
-	max_filt_u(test_tab, test_tab_2, 4, 4, 2);
-	printf("finished filtering\n");
+	int width = al_get_bitmap_width(image);
+	int height = al_get_bitmap_height(image);
+	display = al_create_display(width, height);
 
-	for (int j = 0; j < 48; ++j) {
-		if(j%12 == 0)
-			printf("\n");
-		printf("%d ", test_tab_2[j]);
+	int byteW = width * 3;
+	unsigned char *in_buf = (unsigned char *)malloc(height * byteW);
+
+	region = al_lock_bitmap(image,ALLEGRO_PIXEL_FORMAT_RGB_888, ALLEGRO_LOCK_READWRITE);
+	memcpy(in_buf, ((unsigned char *)(region->data)) - (height - 1) * byteW, height * byteW);
+	al_unlock_bitmap(image);
+
+	while (true){
+		printf("type in mask size ");
+		int mask;
+		scanf("%d", &mask);
+		region = al_lock_bitmap(image, ALLEGRO_PIXEL_FORMAT_RGB_888, ALLEGRO_LOCK_READWRITE);
+
+		max_filt(in_buf, (unsigned char *)(region->data) - (height - 1) * byteW, width, height, mask);
+
+		al_unlock_bitmap(image);
+		al_draw_bitmap(image, 0, 0, 0);
+		al_flip_display();
+
+		printf("Do you want to filter with anoter mask size?(y/n) ");
+		scanf("%s", comm);
+		if (comm[0] != 'y')
+			break;
 	}
-//
-//	al_unlock_bitmap(image);
-//
-//	al_draw_bitmap(image, 0, 0, 0);
-//	al_flip_display();
-//
-//	printf("Press eny key to finish ");
-//	char control[10];
-//	scanf("%s", control);
-//	free(input_buffer);
-//
-//	al_destroy_display(display);
-//	al_destroy_bitmap(image);
-//	al_shutdown_image_addon();
+
+	free(in_buf);
+	al_destroy_display(display);
+	al_destroy_bitmap(image);
+	al_shutdown_image_addon();
 	return 0;
+}
+
+void init_pix_format(){
+	al_set_new_bitmap_format(ALLEGRO_PIXEL_FORMAT_BGR_888);
+	if (!al_init())	{
+		al_show_native_message_box(display, "Error", "Error", "Failed allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		exit(1);
+	}
+
+	if (!al_init_image_addon()){
+		al_show_native_message_box(display, "Error", "Error", "ad fail!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		exit(1);
+	}
 }
--- makefile
+++ makefile
@@ -1,13 +1,10 @@
 LIB=`pkg-config --libs allegro-5 allegro_image-5 allegro_dialog-5`
 
-all: clean add.o uint.o
-	 gcc -g main.c add.o uint.o -o main.o $(LIB)
+all: clean max.o
+	 gcc -g main.c max.o -o main.o $(LIB)
 
-add.o:
-	 nasm -g -f elf64 add_two.s -o add.o
-
-uint.o:
-	nasm -g -f elf64 uint_filter.s -o uint.o
+max.o:
+	 nasm -g -f elf64 max_filter.s -o max.o
 
 clean:
 	@rm -f *.o
